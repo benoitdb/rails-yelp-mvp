b8ba4fb5ba8d28a40cff73fd982a689eb096884c
Benoit Dejean <benoit.dejeandelabatie@gmail.com>
2018-03-28 17:04:07 +0200

    Adding models routes controllers and views for restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do
+Restaurant.create!(
+  name: Faker::Hipster.sentence(3),
+  address: ['Nantes', 'Paris', 'London', 'Seattle', 'Brussels'].sample,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+)
+end
+
+puts 'Creating reviews...'
+10.times do
+Review.create!(
+  restaurant_id: [0, 1, 2, 3, 4, 5].sample,
+  content: Faker::Hipster.sentence(4),
+  rating: [0, 1, 2, 3, 4, 5].sample,
+)
+end
+
+puts 'Finished!'
